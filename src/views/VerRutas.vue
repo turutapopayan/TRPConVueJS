4db3d556785662b5f2a7383c783c56bb35d9a877
verpal20 <verpalechor@misena.edu.co>
2021-11-16 20:45:27 -0500

    Titulo

--- src/views/VerRutas.vue
+++ src/views/VerRutas.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Menu/>
+    <h1 class="Titulo">RUTAS</h1>
+    <body>
+      
+    </body>
   </div>
 
 </template>
@@ -16,3 +20,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+*{
+  margin: 40px;
+  border: 5px solid blue;
+}
+.Titulo{
+  border: 1px solid red;
+  text-align: center;
+}
+
+</style>
